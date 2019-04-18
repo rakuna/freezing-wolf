d85b3d1e312d5806c64be0859d76b149ff6e1af2
Tom McCleery <mccleery.tom@gmail.com>
2019-04-18 22:04:06 +0800

    Update 2019-04-18 Update: - post_install script with more programes
    Include:
    - Lineage flashing software (udev) script

--- scripts/post_install.py
+++ scripts/post_install.py
@@ -2,10 +2,10 @@ import subprocess
 
 def update():
     print('******update******')
-    subprocess.run(['apt', 'update'])
+    subprocess.run(['sudo', 'apt', 'update'])
     print('******')
-    print(subprocess.CompletedProcess)
-    subprocess.run(['apt', 'upgrade'])
+    # print(subprocess.CompletedProcess)
+    subprocess.run(['sudo', 'apt', 'upgrade'])
 
 def setup_swap():
     print('******swap******')
@@ -15,43 +15,81 @@ def setup_swap():
     permissions = '600'    # owner can read/write
     fstab_file = '/etc/fstab'
 
-    # TODO
-    # if swap_file does not exist then run the following
-    # else exit this function.
+
+
+    # TODO check if the file exists
 
     # commands
-    # make the file
     print('fallocate')
-    subprocess.run(['fallocate', '-l', swap_size, swap_file])
+    # if swap_file does not exist then run the following
+    try:
+        # make the file
+        subprocess.run(['sudo', 'fallocate', '-l', swap_size, swap_file])
 
-    # make the file a swap file
-    print('mkswap')
-    subprocess.run(['mkswap', swap_file])
+        # make the file a swap file
+        print('mkswap')
+        subprocess.run(['sudo', 'mkswap', swap_file])
 
-    # assign the swap file restricted permissions
-    print('chmod')
-    subprocess.run(['chmod', permissions, swap_file])
+        # assign the swap file restricted permissions
+        print('chmod')
+        subprocess.run(['sudo', 'chmod', permissions, swap_file])
 
-    # turn the swap file on
-    print('swapon')
-    subprocess.run(['swapon', swap_file])
+        # turn the swap file on
+        print('swapon')
+        subprocess.run(['sudo', 'swapon', swap_file])
 
-    # update fstab to turn swap on after every reboot
-    print('fstab')
-    with open(fstab_file, 'a') as fstab:
-        fstab.write('var/swap swap swap defaults 0 0')
+        # update fstab to turn swap on after every reboot
+        print('fstab')
+        with open(fstab_file, 'a') as fstab:
+            fstab.write('var/swap swap swap defaults 0 0')
 
-    # show current swap status (for removing other swap files)
-    print('swap status')
-    subprocess.run(['swapon', '--summary'])
+        # show current swap status (for removing other swap files)
+        print('swap status')
+        subprocess.run(['sudo', 'swapon', '--summary'])
 
-def install_applications():
+    except:
+        # else exit this function.
+        raise
+        return
+
+
+def install_apt_applications():
+    """Install applications that are available in default apt."""
     print('******install******')
-    applications = ['git']
+    applications = ['git',  # App for version control.
+                    'geary',  # App for non-gmail email
+                    'evolution',  # App for gmail email.
+                    'quodlibet',  # App for music.
+                    'gnome-tweaks',  # App for finer grained desktop style.
+                    'chrome-gnome-shell',  # App linking firefox with tweaks.
+                    'syncthing',  # App for managing back-ups and syncing.
+                   ]
     for app in applications:
         print('******'+app+'******')
-        subprocess.run(['apt', 'install', app])
+        subprocess.run(['sudo', 'apt', 'install', app])
+
+
+def install_non_apt_applications():
+    """Install all applications that are not available in apt by default."""
+
+    # balenaEtcher (https://github.com/balena-io/etcher)
+    print('******baleneEtcher******')
+    # Add Etcher debian repository:
+    balena_etcher_source_file = '/etc/apt/sources.list.d/balena-etcher.list'        
+    with open(balena_etcher_source_file, 'w') as balena_etcher_source:
+        balena_etcher_source.write("deb https://deb.etcher.io stable etcher")
+
+    # Trust Bintray.com's GPG key:    
+    subprocess.run(['sudo', 'apt-key', 'adv',
+                    '--keyserver', 'keyserver.ubuntu.com',
+                    '--recv-keys', '379CE192D401AB61'])
+
+    # Update and install
+    subprocess.run(['sudo', 'apt', 'update'])
+    subprocess.run(['sudo', 'apt', 'install', 'balena-etcher-electron'])
 
 update()
 setup_swap()
-install_applications()
+install_apt_applications()
+install_non_apt_applications()
+
